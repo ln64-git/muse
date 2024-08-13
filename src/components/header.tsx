2a5506ad179d3164ae36bc1507eb6326e0d045a2
ln64 <ln64.ohio@gmail.com>
2024-08-12 22:39:34 -0400

    dark ui

---
+++ src/components/header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <>
+      <div className="bg-neutral-950 h-[76px] flex items-center">
+        <button className="p-2 ml-2 text-xl rounded-lg transition-all duration-300 transform hover:scale-110 hover:bg-neutral-900 hover:shadow-lg ">
+          Muse
+        </button>
+      </div>
+    </>
+  );
+}
---
+++ src/components/sidebar.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import React from "react";
+
+function SidebarIcon({ src }: { src: string }) {
+  return (
+    <div className="min-w-[40px] bg-neutral-900 p-2 m-4 rounded-lg transition-all duration-300 transform hover:scale-110 hover:bg-neutral-800 hover:shadow-lg  ">
+      <Image src={src} width={50} height={50} alt="Icon" />
+    </div>
+  );
+}
+
+export default function Sidebar() {
+  return (
+    <div className="h-full bg-neutral-950 w-[80px] pt-[75px]">
+      <SidebarIcon src="/sidebar/library.svg" />
+      <SidebarIcon src="/sidebar/playlist.svg" />
+      <SidebarIcon src="/sidebar/artist.svg" />
+      <SidebarIcon src="/sidebar/album.svg" />
+      <SidebarIcon src="/sidebar/songs.svg" />
+    </div>
+  );
+}
