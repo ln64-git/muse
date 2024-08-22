3f4c3d6e31df6bd49b5f54f1230bc4e91417a0b5
ln64 <ln64.ohio@gmail.com>
2024-08-22 02:41:33 -0400

    subtle changes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,16 +7,16 @@ export default function Header() {
   return (
     <div
       onClick={() => setCurrentPage("library")}
-      className="bg-neutral-950 h-[76px] flex justify-between items-center px-4"
+      className="bg-neutral-950 h-[76px] flex justify-between items-center pl-2 pr-4"
     >
-      <button className="text-xl text-neutral-400 p-2 m-3 rounded-lg transition-all duration-300 transform hover:scale-110 hover:bg-neutral-900 hover:shadow-lg hover:brightness-150">
+      <button className="text-xl text-neutral-400 p-2 rounded-lg transition-all duration-300 transform hover:scale-110 hover:bg-neutral-900 hover:shadow-lg hover:brightness-150">
         Muse
       </button>
       <div className="flex">
-        <div className="min-w-[45px] h-[50px] w-[50px] flex items-center justify-center p-2  rounded-lg transition-all duration-300 transform hover:scale-110 hover:bg-neutral-900 hover:shadow-lg hover:brightness-150">
+        <div className="min-w-[45px] cursor-pointer h-[50px] w-[50px] flex items-center justify-center p-2  rounded-lg transition-all duration-300 transform hover:scale-110 hover:bg-neutral-900 hover:shadow-lg hover:brightness-150">
           <SearchIcon className="fill-current text-neutral-600" size={25} />
         </div>
-        <div className="min-w-[45px] h-[50px] w-[50px] flex items-center justify-center p-2  rounded-lg transition-all duration-300 transform hover:scale-110 hover:bg-neutral-900 hover:shadow-lg hover:brightness-150">
+        <div className="min-w-[45px] cursor-pointer h-[50px] w-[50px] flex items-center justify-center p-2  rounded-lg transition-all duration-300 transform hover:scale-110 hover:bg-neutral-900 hover:shadow-lg hover:brightness-150">
           <VolumeHighIcon className="fill-current text-neutral-600" size={25} />
         </div>
       </div>
