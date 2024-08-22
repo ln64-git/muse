1b53dd8bb3d0bdaf02be0b7716a60c667da57b39
ln64 <ln64.ohio@gmail.com>
2024-08-22 02:37:41 -0400

    header button styling

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,14 +9,14 @@ export default function Header() {
       onClick={() => setCurrentPage("library")}
       className="bg-neutral-950 h-[76px] flex justify-between items-center px-4"
     >
-      <button className="text-xl hover:filter hover:brightness-120 rounded-lg transition-all duration-300 transform hover:scale-110 hover:bg-neutral-900 hover:shadow-lg">
+      <button className="text-xl text-neutral-400 p-2 m-3 rounded-lg transition-all duration-300 transform hover:scale-110 hover:bg-neutral-900 hover:shadow-lg hover:brightness-150">
         Muse
       </button>
       <div className="flex">
-        <div className="25 h-[50px] w-[50px] flex items-center justify-center rounded-lg">
+        <div className="min-w-[45px] h-[50px] w-[50px] flex items-center justify-center p-2  rounded-lg transition-all duration-300 transform hover:scale-110 hover:bg-neutral-900 hover:shadow-lg hover:brightness-150">
           <SearchIcon className="fill-current text-neutral-600" size={25} />
         </div>
-        <div className="25 h-[50px] w-[50px] flex items-center justify-center rounded-lg">
+        <div className="min-w-[45px] h-[50px] w-[50px] flex items-center justify-center p-2  rounded-lg transition-all duration-300 transform hover:scale-110 hover:bg-neutral-900 hover:shadow-lg hover:brightness-150">
           <VolumeHighIcon className="fill-current text-neutral-600" size={25} />
         </div>
       </div>
